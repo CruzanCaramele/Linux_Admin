e839b2b6e789919e188fac411ab58e3a79603b74
cruzancaramele <letters2hamza@gmail.com>
2015-10-03 16:41:33 +0000

    create if statement to check the existence of the backup.zip folder

--- backup.py
+++ backup.py
@@ -6,3 +6,13 @@ import logging
 # create the file where logs are saved
 logging.basicConfig(filename="file_ex.log", level=logging.DEBUG)
 
+logging.info("checking to see if the backup.zip exists")
+
+# check to see if backup file exists
+if os.path.exists("backup.zip"):
+    logging.info("The file Exists!")
+    try:
+        zip_file = zipfile.ZipFile("backup.zip", "a")
+    except:
+	err = sys.exc_info()
+
