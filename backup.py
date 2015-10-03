04e1e462d8563e8bd3411d3d99135c80ddaf9ca7
cruzancaramele <letters2hamza@gmail.com>
2015-10-03 17:04:25 +0000

    Create zip file if it does not exist

--- backup.py
+++ backup.py
@@ -15,4 +15,26 @@ if os.path.exists("backup.zip"):
         zip_file = zipfile.ZipFile("backup.zip", "a")
     except:
 	err = sys.exc_info()
+	logging.error("Unable to open backup.zip in append mode!")
+	logging.error("Error Num: " + str(err[1].args[0]))
+	logging.error("Error Msg: " + err[1].args[1]
+	sys.exit()
+
+else:
+    logging.info("creating backup.zip")
+    try:
+	zip_file = zipfile.ZipFile("backup.zip", "w")
+    except:
+	err = sys.exc_info()
+	logging.error("Unable to crate backup.zip!")
+	logging.error("Error Num: " + str(err[1].args[0]))
+	logging.error("Error Msg: " + err[1].args[1]
+	sys.exit()
+
+logging.info("Adding test.text to backup.zip")
+
+try:
+    zip_file.write("test.txt", "test.txt", zipfile.ZIP_DEFLATED)
+
+except:
 
